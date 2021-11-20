9a2baad8ae3efff011f52221eced9cb3e5a5734b
Simon Krisel <simon.krissel@student.hs-rm.de>
2021-11-20 09:56:16 -0800

    Relationships

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,6 +1,6 @@
 class Collection < ApplicationRecord
     has_and_belongs_to_many :tags
-    belongs_to :user
+    belongs_to :owner, :class_name => "User", :foreign_key => 'owner_id'
     has_and_belongs_to_many :images
     has_many :ratings, as: :rateable
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -3,5 +3,5 @@ class Image < ApplicationRecord
     has_many :ratings, as: :rateable, dependent: :destroy
     has_and_belongs_to_many :collections
     has_and_belongs_to_many :tags
-    belongs_to :owner, class_name: :user
+    belongs_to :owner, :class_name => "User", :foreign_key => 'owner_id'
 end
