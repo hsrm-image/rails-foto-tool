b3db693a740bdb7372f8d8f4d07e9ee0289ae494
Simon Krisel <simon.krissel@student.hs-rm.de>
2021-11-22 09:29:56 -0800

    Very basic Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   resources :tags
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "images#index"
 end
