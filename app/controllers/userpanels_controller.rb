ba588d96d2b2b766270610b1c4b0f3e0654ed92d
lmaos-txt <mail@lmaos.de>
2021-12-13 15:16:43 +0100

    Images Partials now work

--- app/controllers/userpanels_controller.rb
+++ app/controllers/userpanels_controller.rb
@@ -1,8 +1,8 @@
 class UserpanelsController < ApplicationController
     def index
     end
-
-    def show_images
+    def show_images 
+        @images = Image.all
         respond_to do |format|
             format.js{}
           end
