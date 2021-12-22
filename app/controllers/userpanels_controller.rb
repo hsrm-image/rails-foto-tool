71847488d5782dc9561d16b784c5040909377aec
lmaos-txt <mail@lmaos.de>
2021-12-22 01:23:25 +0100

    details view gets the correct data now

--- app/controllers/userpanels_controller.rb
+++ app/controllers/userpanels_controller.rb
@@ -1,15 +1,15 @@
 class UserpanelsController < ApplicationController
-    def index
-    end
-    def show_images 
-        @images = Image.all
-        respond_to do |format|
-            format.js{}
-          end
-    end
-    def show_collections
-        respond_to do |format|
-            format.js{}
-          end
-    end
+	def index; end
+	def show_images
+		@images = Image.all
+		respond_to { |format| format.js {} }
+	end
+	def show_collections
+		respond_to { |format| format.js {} }
+	end
+	def show_details
+		print params['img']
+		@image = Image.find(params['img'])
+		respond_to { |format| format.js {} }
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :comments
-  resources :ratings
-  resources :collections
-  resources :images
-  resources :tags
-  resources :users
-  get :userpanel, to: 'userpanels#index'
-  get 'userpanels/show_images' , to: 'userpanels#show_images'
-  get 'userpanels/show_collections' , to: 'userpanels#show_collections'
-  resources :userpanels
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "images#index"
+	devise_for :users
+	resources :comments
+	resources :ratings
+	resources :collections
+	resources :images
+	resources :tags
+	resources :users
+	get :userpanel, to: 'userpanels#index'
+	get 'userpanels/show_images', to: 'userpanels#show_images'
+	get 'userpanels/show_collections', to: 'userpanels#show_collections'
+	get 'userpanels/show_details', to: 'userpanels#show_details'
+	resources :userpanels
+
+	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+	root to: 'images#index'
 end
