68134df15fecb56a818264d3864a88cb0f085196
Simon Krisel <simon.krissel@student.hs-rm.de>
2022-01-01 06:27:42 -0800

    Image model tests

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -5,15 +5,75 @@ class ImageTest < ActiveSupport::TestCase
     @image = images(:one)
   end
 
-  test "should get ratings for image" do
-    assert_equal Rating.where(rateable_id: @image.id, rateable_type: "Image"), @image.get_ratings
+  def new_image
+    image = Image.new()
+    image.image_file = images(:one).image_file.blob
+    image.owner = images(:one).owner
+    image.save!
+    return image
   end
 
-  test "should get score for image" do
-    sum = 0
-    @image.get_ratings.each{ |x| sum += x.rating }
-    rating = 1.0 * sum / @image.get_ratings.count
-    assert_equal @image.get_score, rating
+  def random_rate_image(image, n=10)
+    ratings = []
+    score = 0.0
+    n.times do |x|
+      rating = Rating.new()
+      rating.rateable = image
+      rating.rating = rand(1..5)
+      rating.session_id = SecureRandom.hex(16)
+      rating.save!
+
+      ratings << rating
+      score += rating.rating
+    end
+    score /= ratings.count
+
+    return ratings, score
+  end
+
+  test "should get ratings for image" do
+    @image = new_image
+    ratings, score = random_rate_image(@image, 10)
+
+    assert_not_nil @image.get_ratings
+    assert_equal ratings, @image.get_ratings
   end
   
+  test "should get total score for image" do
+    @image = new_image
+    ratings, score = random_rate_image(@image, 10)
+
+    assert_equal score, @image.get_score
+  end
+
+  test "should get rate for one user" do
+    @image = new_image
+    ratings, score = random_rate_image(@image, 10)
+    rate = ratings.sample
+
+    assert @image.has_rated?(rate.session_id)
+    assert !@image.has_rated?(SecureRandom.hex(16))
+
+    assert_equal rate, @image.get_rate(rate.session_id)
+    assert_nil @image.get_rate(SecureRandom.hex(16))
+  end
+
+  test "should skip not processed images as visitor" do
+    images.each do |img|
+      assert_not_equal img.next(nil), images(:processing)
+      assert_not_equal img.previous(nil), images(:processing)
+    end
+  end
+
+  test "should navigate to all images as user" do
+    nexts = []
+    prevs = []
+    images.each do |img|
+      nexts << img.next(true)
+      prevs << img.previous(true)
+    end
+
+    assert_includes nexts, images(:processing)
+    assert_includes prevs, images(:processing)
+  end
 end
