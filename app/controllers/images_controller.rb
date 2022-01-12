03619f03c9ba911dd9efb3ec77cfd824fc9ad9f0
Simon Krisel <simon.krissel@student.hs-rm.de>
2022-01-12 02:30:16 -0800

    Fixed images_controller

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,79 +1,106 @@
 class ImagesController < ApplicationController
-	before_action :set_image, only: %i[show edit update destroy]
+	include Authenticate
+	before_action :set_image, only: %i[show edit update destroy analyse]
+	before_action :authenticate_user_custom!,
+	              only: %i[edit update destroy analyse new create]
 
-  # GET /images or /images.json
-  def index
-    if current_user
-      @images = Image.order(:created_at).page(params[:page]) 
-    else
-      @images = Image.where(:processed => true).order(:created_at).page(params[:page]) 
-    end
-  end
+	# GET /images or /images.json
+	def index
+		if current_user
+			@images = Image.order(:created_at).page(params[:page])
+		else
+			@images =
+				Image
+					.where(processed: true)
+					.order(:created_at)
+					.page(params[:page])
+		end
+	end
 
-  # GET /images/1 or /images/1.json
-  def show
-    if current_user or Image.find(params[:id]).processed
-      @image = Image.find(params[:id])
-      @comment = @image.comments.new
-     else
-      respond_to do |format|
-        format.html { redirect_to images_url, notice: "This image is not visible" }
-      end
-    end
-  end
+	# GET /images/1 or /images/1.json
+	def show
+		if current_user or Image.find(params[:id]).processed
+			@image = Image.find(params[:id])
+			@comment = @image.comments.new
+		else
+			respond_to do |format|
+				format.html do
+					redirect_to images_url, notice: 'This image is not visible'
+				end
+			end
+		end
+	end
 
-  # GET /images/new
-  def new
-    @image = Image.new
-  end
+	# GET /images/new
+	def new
+		@image = Image.new
+	end
 
-  # GET /images/1/edit
-  def edit
-  end
+	# GET /images/1/edit
+	def edit; end
 
-  # POST /images or /images.json
-  def create
+	# POST /images or /images.json
+	def create
 		@image = Image.new(image_params)
 		@image.owner = current_user
 		@image.title = image_params[:image_file].original_filename
-     
-    respond_to do |format|
-      if @image.save
-        format.html { redirect_to @image, notice: "Image was successfully created." }
-        format.json { render :show, status: :created, location: @image }
-        AnalyseImageJob.perform_later @image 
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @image.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
+		respond_to do |format|
+			if @image.save
+				format.html do
+					redirect_to @image,
+					            notice: 'Image was successfully created.'
+				end
+				format.json { render :show, status: :created, location: @image }
+				AnalyseImageJob.perform_later @image
+			else
+				format.html { render :new, status: :unprocessable_entity }
+				format.json do
+					render json: @image.errors, status: :unprocessable_entity
+				end
+			end
+		end
+	end
 
-  def analyse
-    respond_to do |format|
-      if @image.image_file.attached?
-        AnalyseImageJob.perform_later @image
-        format.html { redirect_to @image, notice: "Image will be analysed in background." }
-        format.js {render 'layouts/toast', locals: { :method => "success", :message => "Now re-analysing exif-data in Background. Please refresh this page in a few Seconds.", :title => ""}}
-      else
-        format.html { redirect_to @image, notice: "No image!" }
-        format.js {render 'layouts/toast', locals: { :method => "error", :message => "Error: No image attached!", :title => ""}}
-      end
-    end
-  end
+	def analyse
+		respond_to do |format|
+			if @image.image_file.attached?
+				AnalyseImageJob.perform_later @image
+				format.html do
+					redirect_to @image,
+					            notice: 'Image will be analysed in background.'
+				end
+				format.js do
+					render 'layouts/toast',
+					       locals: {
+							method: 'success',
+							message:
+								'Now re-analysing exif-data in Background. Please refresh this page in a few Seconds.',
+							title: '',
+					       }
+				end
+			else
+				format.html { redirect_to @image, notice: 'No image!' }
+				format.js do
+					render 'layouts/toast',
+					       locals: {
+							method: 'error',
+							message: 'Error: No image attached!',
+							title: '',
+					       }
+				end
+			end
+		end
+	end
 
-
-  # PATCH/PUT /images/1 or /images/1.json
-  def update
+	# PATCH/PUT /images/1 or /images/1.json
+	def update
 		respond_to do |format|
 			if @image.update(image_params)
 				format.html { redirect_to userpanels_path, format: 'js' }
 			end
 		end
-  end
-
-
+	end
 
 	# DELETE /images/1 or /images/1.json
 	def destroy
