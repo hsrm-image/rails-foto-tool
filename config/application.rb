fd1132f441be6cb00e0726b3ffb0c0f09b60532b
Simon Krisel <simon.krissel@student.hs-rm.de>
2022-01-12 11:14:06 -0800

    Added translations. Not done yet

--- config/application.rb
+++ config/application.rb
@@ -1,31 +1,31 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module RailsFotoTool
-  class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+	class Application < Rails::Application
+		# Initialize configuration defaults for originally generated Rails version.
+		config.load_defaults 6.1
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+		# Configuration for the application, engines, and railties goes here.
+		#
+		# These settings can be overridden in specific environments using the files
+		# in config/environments, which are processed later.
+		#
+		# config.time_zone = "Central Time (US & Canada)"
+		# config.eager_load_paths << Rails.root.join("extras")
 
-    # Set the maximum filesize for images
-    config.x.image.max_file_size = 5.megabyte
+		# Set the maximum filesize for images
+		config.x.image.max_file_size = 5.megabyte
 
-    # Please edit this variable to reflect the URL of the Server
-    config.x.mail.root_url = "localhost:3000"
+		# Please edit this variable to reflect the URL of the Server
+		config.x.mail.root_url = 'localhost:3000'
 
-    # The mail address the Server should send mails from
-    config.x.mail.sender_address = "please@change-me.com"
-  end
+		# The mail address the Server should send mails from
+		config.x.mail.sender_address = 'please@change-me.com'
+	end
 end
