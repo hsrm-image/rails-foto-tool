4ee7d4c5c6953d29ab8510c6746de50c659f5e1b
Simon Krisel <simon.krissel@student.hs-rm.de>
2022-01-13 07:20:26 -0800

    Further translations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,37 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-    has_many :images, dependent: :destroy, foreign_key: "owner_id"
-    has_many :collections, dependent: :destroy, foreign_key: "owner_id"
-    has_many :comments
+	# Include default devise modules. Others available are:
+	# :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+	devise :invitable,
+	       :database_authenticatable,
+	       :registerable,
+	       :recoverable,
+	       :rememberable,
+	       :validatable
+	has_many :images, dependent: :destroy, foreign_key: 'owner_id'
+	has_many :collections, dependent: :destroy, foreign_key: 'owner_id'
+	has_many :comments
 
-    # Activerecord
-    has_one_attached :avatar 
+	# Activerecord
+	has_one_attached :avatar
 
-    
-    def is_last_admin
-      admin && User.where(:admin => true).count <= 1
-    end
+	def is_last_admin
+		return false unless admin
+		return false unless invitation_accepted?
+		admins = User.where(admin: true)
+		n_admins = 0
+		admins.each { |x| n_admins += 1 if x.invitation_accepted? }
+		return n_admins <= 1
+	end
 
-    def can_revoke_admin(current_user)
-      errors.add(:user, "Cannot revoke permissions of last Admin") if is_last_admin
-      errors.add(:user, "Cannot edit your own Permissions") if id == current_user[:id]
-      return errors.empty?
-    end
+	def can_revoke_admin(current_user)
+		errors.add(:user, I18n.t('users.model.delete_last')) if is_last_admin
+		if id == current_user[:id]
+			errors.add(:user, I18n.t('users.model.edit_own'))
+		end
+		return errors.empty?
+	end
+
+	def invitation_accepted?
+		!invitation_accepted_at.nil? or invited_by_id.nil?
+	end
 end
