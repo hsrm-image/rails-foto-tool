5ee9409131be6d914f10e5fc7c43bea5b8e801bf
Simon Krisel <simon.krissel@student.hs-rm.de>
2022-01-13 08:13:54 -0800

    Language selector

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,25 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
+	before_action :configure_permitted_parameters, if: :devise_controller?
+	around_action :switch_locale
 
-    protected
-  
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    end
+	def switch_locale(&action)
+		locale =
+			params[:locale] || extract_locale_from_accept_language_header ||
+				I18n.default_locale
+		I18n.with_locale(locale, &action)
+	end
+
+	def default_url_options
+		{ locale: I18n.locale }
+	end
+
+	protected
+
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+	end
+
+	def extract_locale_from_accept_language_header
+		request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+	end
 end
--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,12 @@ module RailsFotoTool
 		# config.time_zone = "Central Time (US & Canada)"
 		# config.eager_load_paths << Rails.root.join("extras")
 
+		# Set the available locales
+		I18n.available_locales = %i[en de]
+
+		# Set the default locale
+		I18n.default_locale = :en
+
 		# Set the maximum filesize for images
 		config.x.image.max_file_size = 5.megabyte
 
